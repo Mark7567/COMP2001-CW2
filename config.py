3cb7eba66ea027e1e9fb24ca41f83052b6280532
Peanut7567 <markjh1808@gmail.com>
2024-11-27 15:16:17 +0000

    Minor changes

--- config.py
+++ config.py
@@ -7,17 +7,16 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mssql+pyodbc://?odbc_connect="
-    "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=dist-6-505.uopnet.plymouth.ac.uk;"
-    "DATABASE=COMP2001_MHardy;"
-    "UID=MHardy"
-    "PWD=OwhO778+"
-    "TrustServerCertificate=yes;"
-    "Encrypt=yes;"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mssql+pyodbc:///?odbc_connect='
+    'DRIVER={ODBC Driver 18 for SQL Server};'
+    'SERVER=DIST-6-505.uopnet.plymouth.ac.uk;'
+    'DATABASE=COMP2001_MHardy;'
+    'UID=MHardy;'
+    'PWD=OwhO778+;'
+    'Encrypt=Yes;'
+    'TrustServerCertificate=Yes;'
+    )
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
